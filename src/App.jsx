6e1db21d60b1e1c92fffe8607245aaa6af46ac07
AssassinOFsilchar <abhiran14082002@gmail.com>
2024-07-14 12:14:26 +0530

    oh yes

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <div>
+        Git Link Manager For <a href="https://github.com/Abhishek-Ranjan-16/Travel-Buddy.git">Travel Buddy</a>
+      </div>
+      <p>
+        Below are the github links of the features that are implemented in travel buddy along with the homepage link.
+      </p>
+      <ul>
+        <li><a href="https://github.com/Abhishek-Ranjan-16/expenseTracker.git">Expense Tracker</a></li>
+        <li><a href="https://github.com/Abhishek-Ranjan-16/rpg.git">Password Generator</a></li>
+        <li><a href="https://github.com/Abhishek-Ranjan-16/pass-manager.git">Password Manager</a></li>
+        <li><a href="https://github.com/Abhishek-Ranjan-16/TodoFrontend.git">Todo List</a></li>
+        <li><a href="https://github.com/Abhishek-Ranjan-16/calcium.git">Calculator</a></li>
+        <li><a href="https://github.com/Abhishek-Ranjan-16/memeGenerator.git">Meme Generator</a></li>
+      </ul>
+    </div>
+  );
+};
+
+export default App;
